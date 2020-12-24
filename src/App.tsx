bd6f11ff3176b9c8e17708f587e6d78699041ca8
e <807196833@qq.com>
2020-12-24 10:02:09 +0800

    'add Modal'

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,13 @@
 import './App.css';
 import sMessage from './component/Notification/Notification';
 import {SButton}  from './component/Button/sButton'
+import { useState } from 'react';
+import SModal from './component/Modal/Modal';
   function App() {
+    const [modalvis,setModalVis] =useState(false);
+    const changeModalVis = ()=>{
+        setModalVis(modalvis =>!modalvis);
+    }
     return (
       <div className="App">
         <h1>Notification Demo</h1>
@@ -11,6 +17,24 @@ import {SButton}  from './component/Button/sButton'
         <SButton type="default" size="middle" onClick={()=>sMessage.info("lallalal")}>Primary</SButton>
         <SButton type="primary" size="middle" onClick={()=>sMessage.info("lallalal")}>Primary</SButton>
         <SButton type="default" size="middle" disabled>success</SButton>
+        <SButton type="primary" size="middle" onClick={()=>changeModalVis()}>OpenModal</SButton>
+        <SModal visible={modalvis}
+            title="modalex"
+            footer={
+                [<SButton type='default'>cancel</SButton>,
+                <SButton type='primary'>confirm</SButton>]
+            }
+        >
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p> 
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+        </SModal>
       </div>
     );
   }
